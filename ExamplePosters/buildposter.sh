222243d1b42551d76e54cc5110f79e2bb193e749
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-01-09 22:19:46 +0100

    cleanup

---
+++ ExamplePosters/buildposter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# for bibtex stuff a makefile would be nicer.
+# this file is only here to remind you of the -shell-escape options inforced by the minted package
+pdflatex -shell-escape poster.tex
